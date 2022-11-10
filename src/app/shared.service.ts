1a0519f9674b6345f7b9756709f7b89b5c4e3805
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-11-10 14:51:18 +0600

    first commit

---
+++ src/app/shared.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  readonly APIUrl="http://localhost:50309/";
+
+  constructor(private http:HttpClient) { }
+}
