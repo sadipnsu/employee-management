207133cf9dbfc7f9da778a044612e87b2c05822a
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-11-10 15:30:18 +0600

    Api added from database

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -10,4 +10,43 @@ export class SharedService {
   readonly APIUrl="http://localhost:50309/";
 
   constructor(private http:HttpClient) { }
+
+  //departmentAPI------------>
+  getDepList():Observable<any[]> {
+    return this.http.get<any>(this.APIUrl+'/department');
+  }
+
+  addDepartment(val:any){
+    return this.http.post(this.APIUrl+'/department',val);
+  }
+
+  upDateDepartment(val:any){
+    return this.http.put(this.APIUrl+'/department',val);
+  }
+ 
+  deleteDepartment(val:any){
+    return this.http.delete(this.APIUrl+'/department',val);
+  }
+
+  //EmployeeAPI ------------>
+  getEmpList():Observable<any[]> {
+    return this.http.get<any>(this.APIUrl+'/Employee');
+  }
+
+  addEmployee(val:any){
+    return this.http.post(this.APIUrl+'/Employee',val);
+  }
+
+  upEmployee(val:any){
+    return this.http.put(this.APIUrl+'/Employee',val);
+  }
+ 
+  deleteEmployee(val:any){
+    return this.http.delete(this.APIUrl+'/Employee',val);
+  }
+
+  getAllDepartmentNames():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl+'/Employee/getAllDepartmentNames');
+  }
+
 }
